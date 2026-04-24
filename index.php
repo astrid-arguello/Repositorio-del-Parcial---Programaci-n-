079ff2a8b551028cc63e4cf993dcbaf51e671489
Yessica1995 <yexierr9@gmail.com>
2026-04-24 10:22:45 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Banco - Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Login Banco</h2>
+
+<form action="login.php" method="POST">
+    <input type="text" name="usuario" placeholder="Usuario" required><br>
+    <input type="password" name="password" placeholder="Contraseña" required><br>
+    <button type="submit">Ingresar</button>
+</form>
+
+</body>
+</html>
